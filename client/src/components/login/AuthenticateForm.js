c951747f6e42cebd32a117189202b959a532022e
Dionusia Psiri <66883669+Dionusia@users.noreply.github.com>
2022-12-19 01:02:21 +0200

    Update AuthenticateForm.js

--- client/src/components/login/AuthenticateForm.js
+++ client/src/components/login/AuthenticateForm.js
@@ -18,14 +18,16 @@ const AuthenticateForm = (props)=>{
             const cred =  { email, password}
             axios.post("http://localhost:5000/api/auth", cred)
         .then(res => {
-            if (res.data.user.isAdmin) {
-                localStorage.setItem('token', res.data.user)
-                alert('Login successful')
-                window.location = '/AdminHome'
-            } else if(res.data.user) {
-                localStorage.setItem('token', res.data.user)
-                alert('Login successful')
-                window.location = '/UserHome'
+            if (res.data.user) {
+                if(res.data.user.isAdmin){
+                    localStorage.setItem('token', res.data.user)
+                    alert('Login successful')
+                    window.location = '/AdminHome'
+                }else{
+                    localStorage.setItem('token', res.data.user)
+                    alert('Login successful')
+                    window.location = '/UserHome'
+                }
             }else{
                 alert('Please check your username and password')
             }
