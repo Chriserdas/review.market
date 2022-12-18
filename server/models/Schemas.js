7b323f0b33baa82ffa8d2085abc5f97f40729bd1
Dionusia Psiri <66883669+Dionusia@users.noreply.github.com>
2022-12-18 23:59:41 +0200

    changes

---
+++ server/models/Schemas.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	username:{type:String,required:[true,'Please provide username'],minlength:4,maxlength:25},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:[true,'Please provide password']},
+    isAdmin:{type:Boolean,default:false,require:true},
+    entryDate:{type:Date,default:Date()},
+    token:{type:Number, default:0}
+});
+
+const productSchema = new mongoose.Schema(
+{
+    "products" :[{
+      id: {type:Number},
+      name: { type: String, required: true},
+      category: { type: String, required: true },
+      subcategory: {type: String, required: true}
+
+    },
+],
+    "categories" :[{
+      id: {type:Number},
+      name: { type: String, required: true},
+      subcategory: [{
+        name: { type: String, required: true},
+        uuid: {type:Number, required: true},
+      }]
+    }]
+}
+  );
+
+
+const User = mongoose.model("users", userSchema);
+const Product = mongoose.model("product", productSchema);
+
+module.exports = { User, Product};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
-const { User } = require("../models/user");
+const { User } = require("../models/Schemas");
 const bcrypt = require("bcrypt");
 
 router.post("/", async(req, res)=> {
-	const { name, email, password} = req.body
+    const { name, email, password} = req.body
+    if (!/(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/.test(password)) {
+        res.send( { message: "The password must be at least 8 characters and contain at least one capital letter on number and some symbol (eg #$*&@)." })
+    }
     User.findOne({email: email}, async(err, user) => {
         if(user){
             res.send({message: "User already registerd"})
         } else {
-            const newPassword = await bcrypt.hash(req.body.password, 10)
+            const salt = await bcrypt.genSalt(10)
+            const newPassword = await bcrypt.hash(req.body.password, salt)
             const user = new User({
                 username: req.body.username,
                 email: req.body.email,
