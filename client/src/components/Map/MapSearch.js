42250f29831edde8e2a3021a00d8d4f97ffcdcd0
Dionusia Psirri <66883669+Dionusia@users.noreply.github.com>
2022-12-24 16:51:57 +0200

    changes

--- client/src/components/Map/MapSearch.js
+++ client/src/components/Map/MapSearch.js
@@ -25,70 +25,70 @@ export default function MapSearch() {
     }
     
     
-    function LocationMarker() {
-      const [position, setPosition] = useState(null);
-      const [bbox, setBbox] = useState([]);
-    
-      const map = useMap();
-    
-      useEffect(() => {
-        map.locate().on("locationfound", function (e) {
-          setPosition(e.latlng);
-          map.flyTo(e.latlng, map.getZoom());
-          setBbox(e.bounds.toBBoxString().split(","));
-        });
-      }, [map]);
-    
-      return position === null ? null : (
-        <Marker position={position}>
-          <Popup>
-            You're current location. <br />
-            Map bbox: <br />
-            <b>Southwest lng</b>: {bbox[0]} <br />
-            <b>Southwest lat</b>: {bbox[1]} <br />
-            <b>Northeast lng</b>: {bbox[2]} <br />
-            <b>Northeast lat</b>: {bbox[3]}
-          </Popup>
-        </Marker>
-      );
-    }
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const [bbox, setBbox] = useState([]);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      setBbox(e.bounds.toBBoxString().split(","));
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        You're current location. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
+  );
+}
 
 function Search() {
   const map = useMap();
 
   
-    useEffect(() => {
-        const markersLayer = new L.LayerGroup(); //layer contain searched elements
-        map.addLayer(markersLayer);
-        const controlSearch = new L.Control.Search({
-        position: "topleft",
-        layer: markersLayer,
-        initial: false,
-        zoom: 20,
-        marker: false,
-        });
+  useEffect(() => {
+    const markersLayer = new L.LayerGroup(); //layer contain searched elements
+    map.addLayer(markersLayer);
+    const controlSearch = new L.Control.Search({
+      position: "topleft",
+      layer: markersLayer,
+      initial: false,
+      zoom: 20,
+      marker: false,
+    });
 
-        map.addControl(controlSearch);
+    map.addControl(controlSearch);
 
-        const getPois = async () => {
-        try {
-                const response = await Axios.get("http://localhost:5000/api/supermarket"); 
-                response.data.forEach((item) =>{
-                    let marker = new L.Marker(new L.latLng(item.coordinates[1],item.coordinates[0]), {title: item.name});
-                    marker.bindPopup(
-                        "Name: " + item.features.properties.name
-                    );
-                    markersLayer.addLayer(marker);
-                });
-                
+    const getPois = async () => {
+      try {
+            const response = await Axios.get("http://localhost:5000/api/supermarket"); 
+            response.data.forEach((item) =>{
+                let marker = new L.Marker(new L.latLng(item.coordinates[1],item.coordinates[0]), {title: item.name});
+                marker.bindPopup(
+                    "Name: " + item.features.properties.name
+                );
+                markersLayer.addLayer(marker);
+            });
             
-        } catch (err) {
-            // Handle Error Here
-            console.error(err);
-        }
-        };
-    getPois()
-    },[map]);
+          
+      } catch (err) {
+          // Handle Error Here
+          console.error(err);
+      }
+  };
+  getPois()
+  },[map]);
 
   return null;  //don't want anything to show up from this comp
 }
